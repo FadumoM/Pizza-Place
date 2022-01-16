1b7acc55b67a095ce6360d52f27f2d19089046e5
FadumoM <fadumomohamed011@gmail.com>
2022-01-17 00:41:01 +0300

    about page

--- js/script.js
+++ js/script.js
@@ -9,11 +9,32 @@ Pizza.prototype.price=function(){
   var pizzaCrust =this.crust;
   debugger;
   if(pizzaSize ==="small"){
-    return 5 + (pizzaToppings*2)+(pizzaCrust*2);
+    return 5 + (pizzaToppings+2)+(pizzaCrust+2);
   }else if(pizzaSize ==="mid-size"){
-    return 8 + (pizzaToppings*2)+(pizzaCrust*2);
+    return 8 + (pizzaToppings+2)+(pizzaCrust+2);
   }else if (pizzaSize ==="large"){
-    return 10 + (pizzaToppings*2)+(pizzaCrust*2);
+    return 10 + (pizzaToppings+2)+(pizzaCrust+2);
   }
   
 }
+  //user interface 
+
+$(document).ready(function(){
+  $("form#order-form").submit(function(event){
+    event.preventDefault();
+
+    var orderSize =$('#size').val();
+    var orderToppings =$('#toppings').val();
+    var orderCrust =$('#crust').val();
+
+ var newOrder=new Pizza(orderSize,orderToppings,orderCrust );
+ var orderPrice =newOrder.price().toFixed(2);
+
+ $('.price').text(orderPrice);
+ $('.size').text(newOrder.size);
+ $('.toppings').text(newOrder.toppings);
+ $('.crust').text(newOrder.crust);
+
+ $('#result').hide();
+  });
+}); 

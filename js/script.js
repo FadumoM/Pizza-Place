6d3d456d7d4754c479962c09522a8883b0161aa2
FadumoM <fadumomohamed011@gmail.com>
2022-01-16 23:50:23 +0300

    rectified mistake

--- js/script.js
+++ js/script.js
@@ -4,5 +4,16 @@ function Pizza(size,toppings,crust){
   this.crust=crust;
 }
 Pizza.prototype.price=function(){
-  var 
+  var pizzaSize =this.size;
+  var pizzaToppings =this.toppings;
+  var pizzaCrust =this.crust;
+  debugger;
+  if(pizzaSize ==="small"){
+    return 5 + (pizzaToppings*2)+(pizzaCrust*2);
+  }else if(pizzaSize ==="mid-size"){
+    return 8 + (pizzaToppings*2)+(pizzaCrust*2);
+  }else if (pizzaSize ==="large"){
+    return 10 + (pizzaToppings*2)+(pizzaCrust*2);
+  }
+  
 }

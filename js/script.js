4d87802e28ef9eb1530a69f8dae636bc70483dde
FadumoM <fadumomohamed011@gmail.com>
2022-01-17 17:16:45 +0300

    order fixed

--- js/script.js
+++ js/script.js
@@ -1,40 +1,78 @@
-function Pizza(size,toppings,crust){
-  this.size=size;
-  this.toppings=toppings;
-  this.crust=crust;
-}
-Pizza.prototype.price=function(){
-  var pizzaSize =this.size;
-  var pizzaToppings =this.toppings;
-  var pizzaCrust =this.crust;
-  debugger;
-  if(pizzaSize ==="small"){
-    return 5 + (pizzaToppings+2)+(pizzaCrust+2);
-  }else if(pizzaSize ==="mid-size"){
-    return 8 + (pizzaToppings+2)+(pizzaCrust+2);
-  }else if (pizzaSize ==="large"){
-    return 10 + (pizzaToppings+2)+(pizzaCrust+2);
+$(document).ready(function() {
+  $("#order-details").hide();
+  $("#deliver").hide();
+
+
+  var totalPriceArray = [];
+  function Order(size,toppings,crust, amount) {
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust;
+    this.amount = amount;
+    this.pizzaPrice = 0;
+  }
+  Order.prototype.pizzaCost = function() {
+    if (this.size === "small") {
+      this.pizzaPrice += 500;
+    } else if (this.size === "mid-size") {
+      this.pizzaPrice += 700;
+    } else if (this.size === "large") {
+      this.pizzaPrice += 1000;
+    }
+    if (this.toppings === "bacon") {
+      this.pizzaPrice += 150;
+    } else if (this.toppings === "mushrooms") {
+      this.pizzaPrice += 150;
+    } else if (this.toppings === "chicken") {
+      this.pizzaPrice += 150;
+    }
+    if (this.crust === "thin") {
+      this.pizzaPrice += 100;
+    } else if (this.crust === "thick") {
+      this.pizzaPrice += 100;
+    } 
+    
+  };
+  function Address(address) {
+    this.address = address;
+    this.deliveryAddress = (address);
   }
   
-}
-  //user interface 
-
-$(document).ready(function(){
-  $("form#order-form").submit(function(event){
-    event.preventDefault();
-
-    var orderSize =$('#size').val();
-    var orderToppings =$('#toppings').val();
-    var orderCrust =$('#crust').val();
-
- var newOrder=new Pizza(orderSize,orderToppings,orderCrust );
- var orderPrice =newOrder.price().toFixed(2);
-
- $('.price').text(orderPrice);
- $('.size').text(newOrder.size);
- $('.toppings').text(newOrder.toppings);
- $('.crust').text(newOrder.crust);
-
- $('#result').hide();
+  Order.prototype.finalCost = function() {
+    var cartTotalPrice = [];
+    for (var arrayElement = 0; arrayElement < totalPriceArray.length; arrayElement++) {
+      cartTotalPrice += totalPriceArray[arrayElement];
+    }
+    return cartTotalPrice;
+  };
+  $(".btn.check-out").click(function() {
   });
-}); 
+  $("form#custom-pizza").submit(function(event) {
+    event.preventDefault();
+    var size = $("select#size").val();
+    var toppings = $("select#toppings").val();
+    var crust = $("select#crust").val();
+    var pizzaDetails = (size + " - " + toppings + " - " + crust);
+    var newPizzaOrder = new Order(size,toppings,crust);
+    newPizzaOrder.pizzaCost();
+    totalPriceArray.push(newPizzaOrder.pizzaPrice);
+    
+    $("#final-cost").text(newPizzaOrder.finalCost());
+    $("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
+    
+  });
+  $("#submit-pizza").click(function() {
+    $("#deliver").toggle();
+  });
+
+  $("#checkout-btn").click(function() {
+    $("#order-details").toggle();
+  });
+  $("form#address-form").submit(function(event) {
+    $(".address-form").toggle();
+    event.preventDefault();
+    var address = $("input#location").val();
+    var newAddress = new Address(address);
+    $("#delivery-option").text("Your pizza will be delivered to: " + newAddress.deliveryAddress);
+  });
+});
